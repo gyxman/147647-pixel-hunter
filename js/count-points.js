264358cbb880a99b3325921697ff3c8bb7af95e6
Roman Aleksyutin <r.aleksyutin@tinkoff.ru>
2018-08-23 16:13:58 +0300

    fix first test

--- js/count-points.js
+++ js/count-points.js
@@ -1,6 +1,5 @@
 const QUESTION_COUNT = 10;
 const LIFES_COUNT = 3;
-const POINTS_COUNT = 1150;
 const CURRENT_ANSWER_POINTS = 100;
 const FAST_ANSWER_POINTS = 50;
 const SLOW_ANSWER_POINTS = -50;
@@ -13,8 +12,6 @@ export const countPonts = (array) => {
   let countLifes = LIFES_COUNT - array.filter((elem) => !elem.answer).length;
   if (array.length < QUESTION_COUNT) {
     return -1;
-  } else if (countFastAnswers === 0 && countSlowAnswers === 0 && countLifes === LIFES_COUNT) {
-    return POINTS_COUNT;
   }
   return countCurrentAnswers.length * CURRENT_ANSWER_POINTS + countFastAnswers.length * FAST_ANSWER_POINTS + countSlowAnswers.length * SLOW_ANSWER_POINTS + countLifes * LIFE_POINTS;
 };
