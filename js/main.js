0acd93375fd2320b478e894079114760223a9e0a
Roman Aleksyutin <gyxman@yandex.ru>
2018-08-28 23:04:05 +0300

    add first event

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import {changeScreen} from './utils/util';
 import introScreen from './screens/intro-screen';
 import introData from './data/intro-data';
+import greetingScreen from './screens/greeting-screen';
 
 changeScreen(introScreen(introData));
+
+const nextButton = document.querySelector(`.intro__asterisk`);
+nextButton.addEventListener(`click`, () => {
+  changeScreen(greetingScreen);
+});
