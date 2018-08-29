4ec84f7f45be8decca5d3cba0d5af293d98939b8
Roman Aleksyutin <r.aleksyutin@tinkoff.ru>
2018-08-29 10:18:25 +0300

    work

---
+++ js/data/game1-data.js
@@ -0,0 +1,30 @@
+export default {
+  title: `Угадайте для каждого изображения фото или рисунок?`,
+  options: [
+    `
+    <img src="http://placehold.it/468x458" alt="Option 1" width="468" height="458">
+      <label class="game__answer game__answer--photo">
+        <input class="visually-hidden" name="question1" type="radio" value="photo">
+        <span>Фото</span>
+      </label>
+      <label class="game__answer game__answer--paint">
+        <input class="visually-hidden" name="question1" type="radio" value="paint">
+        <span>Рисунок</span>
+      </label>
+    `,
+    `
+    <img src="http://placehold.it/468x458" alt="Option 2" width="468" height="458">
+      <label class="game__answer  game__answer--photo">
+        <input class="visually-hidden" name="question2" type="radio" value="photo">
+        <span>Фото</span>
+      </label>
+      <label class="game__answer  game__answer--paint">
+        <input class="visually-hidden" name="question2" type="radio" value="paint">
+        <span>Рисунок</span>
+      </label>
+    `
+  ],
+  creationDate: `2016 — 2017`,
+  remainingTime: `30`,
+  lives: 3,
+};
--- js/screens/game1-screen.js
+++ js/screens/game1-screen.js
@@ -1,68 +1,33 @@
-import {changeScreen, getElementFromTemplate} from '../utils/util';
-import game2Screen from './game2-screen';
-import goHome from '../utils/back-intro';
+import {getElementFromTemplate} from '../utils/util';
+import game1Data from '../data/game1-data';
 import getHeader from '../parts/header';
 import getFooter from '../parts/footer';
-import {INITIAL_GAME} from '../data/initial-data';
 
-const template = `
-<section class="game">
-  <p class="game__task">Угадайте для каждого изображения фото или рисунок?</p>
-  <form class="game__content">
-    <div class="game__option">
-      <img src="http://placehold.it/468x458" alt="Option 1" width="468" height="458">
-      <label class="game__answer game__answer--photo">
-        <input class="visually-hidden" name="question1" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer game__answer--paint">
-        <input class="visually-hidden" name="question1" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>
-    </div>
-    <div class="game__option">
-      <img src="http://placehold.it/468x458" alt="Option 2" width="468" height="458">
-      <label class="game__answer  game__answer--photo">
-        <input class="visually-hidden" name="question2" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer  game__answer--paint">
-        <input class="visually-hidden" name="question2" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>
-    </div>
-  </form>
-  <ul class="stats">
-    <li class="stats__result stats__result--wrong"></li>
-    <li class="stats__result stats__result--slow"></li>
-    <li class="stats__result stats__result--fast"></li>
-    <li class="stats__result stats__result--correct"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-    <li class="stats__result stats__result--unknown"></li>
-  </ul>
-</section>
+const template = (data) => `
+  ${getHeader(data)}
+  <section class="game">
+    <p class="game__task">Угадайте для каждого изображения фото или рисунок?</p>
+    <form class="game__content">
+      ${[...data.options].map((option) => `
+        <div class="game__option">${option}</div>
+      `).join(``)}
+    </form>
+    <ul class="stats">
+      <li class="stats__result stats__result--wrong"></li>
+      <li class="stats__result stats__result--slow"></li>
+      <li class="stats__result stats__result--fast"></li>
+      <li class="stats__result stats__result--correct"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--unknown"></li>
+      <li class="stats__result stats__result--unknown"></li>
+    </ul>
+  </section>
+  ${getFooter(data.creationDate)}
 `;
 
-const game1Data = `${getHeader(INITIAL_GAME)} ${template} ${getFooter(2018)}`;
-const game1Element = getElementFromTemplate(game1Data);
-
-const radioElements = game1Element.querySelectorAll(`.visually-hidden`);
-const variants = game1Element.querySelectorAll(`.game__option`);
-const backButton = game1Element.querySelector(`.back`);
-
-goHome(backButton);
-
-radioElements.forEach((element)=> {
-  element.addEventListener(`change`, ()=> {
-    const selectedRadioElements = game1Element.querySelectorAll(`.visually-hidden:checked`);
-    if (selectedRadioElements.length === variants.length) {
-      changeScreen(game2Screen);
-    }
-  });
-});
+const game1Element = getElementFromTemplate(template(game1Data));
 
 export default game1Element;
