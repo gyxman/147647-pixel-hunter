7603da9899c764891c134a53c8bf45c8dd68c50f
Roman Aleksyutin <r.aleksyutin@tinkoff.ru>
2018-08-30 16:06:51 +0300

    work

--- js/screens/game-screen.js
+++ js/screens/game-screen.js
@@ -9,12 +9,37 @@ const initialState = {
   remainingTime: 0,
 };
 
+const getGameTemplate = (data, initialData) => {
+  if (initialData.level === `level-0` || initialData.level === `level-1`) {
+    return `
+      ${[...data.options].map((option, index) => `
+        <div class="game__option">
+          <img src="${option.src}" alt="Option ${index + 1}" width="468" height="458">
+          <label class="game__answer game__answer--photo">
+            <input class="visually-hidden" name="question${index + 1}" type="radio" value="${option.labels[0].value}">
+            <span>${option.labels[0].name}</span>
+          </label>
+          <label class="game__answer game__answer--paint">
+            <input class="visually-hidden" name="question${index + 1}" type="radio" value="${option.labels[1].value}">
+            <span>${option.labels[0].name}</span>
+          </label>
+        </div>
+      `).join(``)}
+    `;
+  }
+  return `
+    ${[...data.options].map((option, index) => `
+      <div class="game__option"><img src="${option.src}" alt="Option ${index + 1}" width="304" height="455"></div>
+    `).join(``)}
+  `;
+};
+
 const template = (data, initialData) => `
   ${getHeader(initialData)}
   <section class="game">
     <p class="game__task">${data.title}</p>
-    <form class="game__content">
-      ${[...data.options].map((option, index) => `
+    <form class="game__content ${initialData.level === `level-1` ? `game__content--wide` : ``}">
+      /*${[...data.options].map((option, index) => `
       <div class="game__option">
         <img src="${option.src}" alt="Option ${index + 1}" width="468" height="458">
         <label class="game__answer game__answer--photo">
@@ -26,7 +51,8 @@ const template = (data, initialData) => `
           <span>${option.labels[0].name}</span>
         </label>
       </div>
-      `).join(``)}
+      `).join(``)}*/
+      ${getGameTemplate(data, initialData)}
     </form>
     <ul class="stats">
       <li class="stats__result stats__result--wrong"></li>
